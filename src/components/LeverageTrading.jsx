f846e67b0017d1bdfad8a89500afaba8c1824596
Priyanka-390 <psr82220@gmail.com>
2024-06-11 13:54:32 +0530

    start

---
+++ src/components/LeverageTrading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeverageTrading = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LeverageTrading
---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,58 @@
+import { Avax, Bnb, BtcUsd, DogeUsd, EthUsd, GreenGraph, Link, PinkGraph } from "./Icons";
+
+export const LeverageCardsData = [
+  {
+    coin: <BtcUsd />,
+    name: "BTCUSD",
+    type: "BTC",
+    graphImg: <PinkGraph />,
+    value: "60942.36",
+    chg: "-3.01%",
+    vol: "$78.07M",
+  },
+  {
+    coin: <EthUsd />,
+    name: "ETHUSD",
+    type: "ETH",
+    graphImg: <GreenGraph />,
+    value: "2,920.34",
+    chg: "-3.72%",
+    vol: "$78.07M",
+  },
+  {
+    coin: <DogeUsd />,
+    name: "DOGEUSD",
+    type: "Dogecoin",
+    graphImg: <GreenGraph />,
+    value: "0.14441",
+    chg: "-4.50%",
+    vol: "$78.07M",
+  },
+  {
+    coin: <Avax />,
+    name: "BTCUSD",
+    type: "USD",
+    graphImg: <PinkGraph />,
+    value: "33.59",
+    chg: "-1.33%",
+    vol: "$78.07M",
+  },
+  {
+    coin: <Link />,
+    name: "LINK",
+    type: "USD",
+    graphImg: <GreenGraph />,
+    value: "13.61",
+    chg: "-5.58%",
+    vol: "$78.07M",
+  },
+  {
+    coin: <Bnb />,
+    name: "BNB",
+    type: "USD",
+    graphImg: <PinkGraph />,
+    value: "586.52",
+    chg: "-4.48%",
+    vol: "$78.07M",
+  },
+];
